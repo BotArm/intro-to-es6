8ba89470002632902d140055a2f3a3aba8472d79
Nattapong Lahoongpase <alone_cow_nl_forever@hotmail.com>
2017-09-02 15:46:35 +0700

    go

--- js/index.js
+++ js/index.js
@@ -2,3 +2,5 @@ const getGithubProfile = () =>
     fetch('https://api.github.com/users/nattapong')
 
     getGithubProfile()
+        .then(response => console.log(response.json()))
+        .catch(e => console.error('Failed while fetching github user profile'))
