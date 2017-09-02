205491f17978e78e3e1ec10ff1d2fcff7e456f23
Nattapong Lahoongpase <alone_cow_nl_forever@hotmail.com>
2017-09-02 15:47:38 +0700

    go

--- js/index.js
+++ js/index.js
@@ -4,3 +4,4 @@ const getGithubProfile = () =>
     getGithubProfile()
         .then(response => console.log(response.json()))
         .catch(e => console.error('Failed while fetching github user profile'))
+        .then(response => console.log('response', response))
